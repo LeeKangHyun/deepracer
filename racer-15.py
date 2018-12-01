589a076dc7052afaa0cd5c30bff094d1b1fb1e1e
nalbam <me@nalbam.com>
2018-11-30 17:47:03 -0800

    15

--- racer-15.py
+++ racer-15.py
@@ -5,14 +5,6 @@ def reward_function (on_track, x, y, distance_from_center, car_orientation, prog
     marker = 0.4 * track_width
 
     if distance_from_center >= 0.0 and distance_from_center <= marker:
-        reward = throttle + progress
-
-    # add steering penalty
-    if abs(steering) > 0.5:
-        reward *= 0.80
-
-    # add throttle penalty
-    if throttle < 0.5:
-        reward *= 0.80
+        reward = throttle
 
     return float(reward)
