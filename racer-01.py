a927a4003d6a9599dc5599ef099752f7ff7f56f0
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-01-30 17:00:36 +0900

    marker

--- racer-01.py
+++ racer-01.py
@@ -1,15 +1,14 @@
 def reward_function (on_track, x, y, distance_from_center, car_orientation, progress, steps, throttle, steering, track_width, waypoints, closest_waypoint):
 
     reward = 1e-3
-    marker = 0.3 * track_width
 
     # distance_from_center as reward
-    if track_width > 0 and distance_from_center <= marker:
+    if on_track and track_width > 0:
         reward = 1 - (distance_from_center / track_width / 2)
 
         # add steering penalty
-        if abs(steering) > 0.7:
-            reward *= 0.8
+        # if abs(steering) > 0.7:
+        #     reward *= 0.8
 
         # add throttle penalty
         if throttle < 0.5:
