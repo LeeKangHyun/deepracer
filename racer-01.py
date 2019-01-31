e92a85c71229f311b716fbad7dd6e69afe195731
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-01-31 12:48:54 +0900

    penalty

--- racer-01.py
+++ racer-01.py
@@ -6,12 +6,12 @@ def reward_function (on_track, x, y, distance_from_center, car_orientation, prog
     if on_track and track_width > 0:
         reward = 1 - (distance_from_center / track_width / 2)
 
-        # add steering penalty
-        # if abs(steering) > 0.7:
-        #     reward *= 0.8
+    # add steering penalty
+    # if abs(steering) > 0.7:
+    #     reward *= 0.5
 
-        # add throttle penalty
-        if throttle < 0.5:
-            reward *= 0.8
+    # add throttle penalty
+    if throttle < 0.5:
+        reward *= 0.5
 
     return float(reward)
