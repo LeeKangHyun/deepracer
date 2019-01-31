4fa503edce4238e4d75e546adeb3e371c5f25012
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-01-31 12:58:25 +0900

    DeepRacerEnv

--- racer-01.py
+++ racer-01.py
@@ -1,17 +1,20 @@
-def reward_function (on_track, x, y, distance_from_center, car_orientation, progress, steps, throttle, steering, track_width, waypoints, closest_waypoint):
+### Gym Env ###
+class DeepRacerEnv():
 
-    reward = 1e-3
+    def reward_function(self, on_track, x, y, distance_from_center, car_orientation, progress, steps,
+                        throttle, steering, track_width, waypoints, closest_waypoints):
+        reward = 1e-3
 
-    # distance_from_center as reward
-    if on_track and distance_from_center > 0 and track_width > 0:
-        reward = 1 - (distance_from_center / (track_width / 2))
+        # distance_from_center as reward
+        if on_track and distance_from_center > 0 and track_width > 0:
+            reward = 1 - (distance_from_center / (track_width / 2))
 
-    # add steering penalty
-    # if abs(steering) > 0.8:
-    #     reward *= 0.5
+        # add steering penalty
+        # if abs(steering) > 0.8:
+        #     reward *= 0.5
 
-    # add throttle penalty
-    if throttle < 0.5:
-        reward *= throttle
+        # add throttle penalty
+        if throttle < 0.5:
+            reward *= throttle
 
-    return float(reward)
+        return float(reward)
