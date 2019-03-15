7d6885de7115ae4073bf5d77ef1561574c7dd69a
nalbam <me@nalbam.com>
2019-03-16 01:57:39 +0900

    1.0

--- matdori-01.py
+++ matdori-01.py
@@ -9,12 +9,12 @@ class DeepRacerEnv():
         if on_track and distance_from_center >= 0 and track_width > 0:
             reward = (1.0 - (distance_from_center / (track_width / 2.0)))
 
-        # add steering penalty
-        if abs(steering) > 0.8:
-            reward *= 0.5
+        # # add steering penalty
+        # if abs(steering) > 0.8:
+        #     reward *= 0.5
 
-        # add throttle penalty
-        if throttle < 0.3:
-            reward *= throttle
+        # # add throttle penalty
+        # if throttle < 0.3:
+        #     reward *= throttle
 
         return float(reward)
