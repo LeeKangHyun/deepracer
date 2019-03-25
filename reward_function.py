8c6b269e850408bf009b08edee2ba6073905bf50
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-03-25 20:32:54 +0900

    rate

--- reward_function.py
+++ reward_function.py
@@ -2,18 +2,24 @@
 
 def reward_function(self, on_track, x, y, distance_from_center, car_orientation, progress, steps,
                     throttle, steering, track_width, waypoints, closest_waypoints):
-    reward = 1e-3
+    reward = 0.001
 
     track_half = track_width * 0.5
 
     if on_track and distance_from_center < track_half:
-        if distance_from_center <= 0.12:
-            reward = 1.0
-        else:
-            reward = 1.0 - (distance_from_center / track_half)
+        rate = distance_from_center / track_half
 
-        if x > 5.6 and steering < 0:
-            reward *= 1.2
+        # reward = 1.0 - rate
+
+        if rate < 0.2:
+            reward = 1.0
+        elif rate < 0.5:
+            reward = 0.5
+        else:
+            reward - 0.1
+
+        # if x > 5.6 and steering < 0:
+        #     reward *= 1.2
 
         # add steering penalty
         if abs(steering) > 0.5:
