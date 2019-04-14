006e4428cb0ea6a9bab248fd39ec23750e3fb9ef
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-04-14 18:40:12 +0900

    mat full

---
+++ mat-1-center.py
@@ -0,0 +1,24 @@
+
+def reward_function(params):
+    import json
+    import math
+
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+
+    distance_rate = distance_from_center / track_width
+
+    reward = 0.001
+
+    if distance_rate <= 0.1:
+        reward = 1.0
+    elif distance_rate <= 0.2:
+        reward = 0.5
+    elif distance_rate <= 0.4:
+        reward = 0.1
+
+    params['log_key'] = 'mat-0'
+    params['reward'] = reward
+    print(json.dumps(params))
+
+    return float(reward)
