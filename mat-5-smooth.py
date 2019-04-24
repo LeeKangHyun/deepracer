52b225fe2f275b5b95e36f43cfaa9660eb24c8ba
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-04-24 15:27:11 +0900

    smooth

--- mat-5-smooth.py
+++ mat-5-smooth.py
@@ -1,9 +1,12 @@
 import json
 import math
 
-CODE_NAME = 'left-angle'
+CODE_NAME = 'smooth'
 
 MAX_ANGLE = math.radians(5)
+
+MAX_STEER = 30
+LEN_STEER = 2
 
 g_progress = 0
 g_episode = 0
@@ -20,28 +23,34 @@ def get_episode(progress, speed):
     global g_total
     global g_bonus
 
+    # reset
     if g_progress > progress:
         g_episode += 1
         g_total = 0
         g_bonus = 0
         del g_steer[:]
     else:
+        # bonus
         if progress == 100:
             g_bonus = 0.5
         else:
             g_bonus = progress - g_progress
 
-    g_progress = progress
-
+    # max speed
     if g_speed < speed:
         g_speed = speed
+
+    # prev progress
+    g_progress = progress
 
     return g_episode
 
 
 def get_diff_angle(coor1, coor2, heading):
+    # guide
     angle = math.atan2((coor2[1] - coor1[1]), (coor2[0] - coor1[0]))
 
+    # car yaw
     yaw = math.radians(heading)
 
     diff = (yaw - angle) % (2.0 * math.pi)
@@ -50,6 +59,26 @@ def get_diff_angle(coor1, coor2, heading):
         diff -= 2.0 * math.pi
 
     return abs(diff)
+
+
+def get_diff_steering(steering):
+    global g_steer
+
+    prev = -100
+    diff = 0
+
+    # steering list
+    g_steer.append(steering)
+    if len(g_steer) > LEN_STEER:
+        del g_steer[0]
+
+    # steering diff
+    for v in g_steer:
+        if prev > -100:
+            diff += abs(prev - v)
+        prev = v
+
+    return diff
 
 
 def reward_function(params):
@@ -63,8 +92,8 @@ def reward_function(params):
 
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    is_left_of_center = params['is_left_of_center']
-    is_reversed = params['is_reversed']
+
+    steering_angle = params['steering_angle']
 
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
@@ -80,26 +109,20 @@ def reward_function(params):
     # diff angle
     diff_angle = get_diff_angle(prev_waypoint, next_waypoint, heading)
 
+    # diff steering
+    diff_steer = get_diff_steering(steering_angle)
+
     if all_wheels_on_track == True:
         # speed
         min_speed = g_speed * 0.7
 
-        # speed and angle
-        if speed >= min_speed and diff_angle <= MAX_ANGLE:
-            # center rate
+        # speed and steer and angle
+        if speed >= min_speed and diff_steer <= MAX_STEER and diff_angle <= MAX_ANGLE:
+            # reward
             distance_reward = 1.1 - (distance_from_center / (track_width / 2))
             angle_reward = 1.1 - (diff_angle / MAX_ANGLE)
 
-            # reverse
-            if is_reversed:
-                if is_left_of_center:
-                    is_left_of_center = False
-                else:
-                    is_left_of_center = True
-
-            # left and angle
-            if is_left_of_center:
-                reward = (distance_reward * angle_reward) + g_bonus
+            reward = (distance_reward * angle_reward) + g_bonus
 
     g_total += reward
 
@@ -107,6 +130,7 @@ def reward_function(params):
     params['log_key'] = CODE_NAME
     params['episode'] = episode
     params['diff_angle'] = diff_angle
+    params['diff_steer'] = diff_steer
     params['reward'] = reward
     params['total'] = g_total
     print(json.dumps(params))
