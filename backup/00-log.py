f45ddb40e3663dcca8e44e809280ac173d44a7c0
nalbam <me@nalbam.com>
2019-05-22 00:42:09 +0900

    add log

--- backup/00-log.py
+++ backup/00-log.py
@@ -1,21 +1,29 @@
 import json
+import time
 
 CODE_NAME = 'center'
 
 g_episode = 0
 g_progress = float(0)
 g_total = float(0)
+g_start = float(0)
+g_time = float(0)
 
 
 def get_episode(progress):
     global g_episode
     global g_progress
     global g_total
+    global g_start
+    global g_time
 
     # reset
     if g_progress > progress:
         g_episode += 1
         g_total = float(0)
+        g_start = time.time()
+
+    g_time = time.time() - g_start
 
     # prev progress
     g_progress = progress
@@ -25,6 +33,7 @@ def get_episode(progress):
 
 def reward_function(params):
     global g_total
+    global g_time
 
     progress = params['progress']
 
@@ -40,6 +49,7 @@ def reward_function(params):
     params['episode'] = episode
     params['reward'] = reward
     params['total'] = g_total
+    params['time'] = g_time
     print(json.dumps(params))
 
     return float(reward)
