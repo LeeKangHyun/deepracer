77fc9cb6ed1d560d934d4640b840c69743b26dc7
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-06-10 11:32:41 +0900

    diff_steps

--- 02 kumo/ku02.py
+++ 02 kumo/ku02.py
@@ -2,8 +2,8 @@ import json
 import math
 import time
 
-NAME = 'ku02-5'
-ACTION = '30 / 7 / 5 / 1'
+NAME = 'ku02-5-5'
+ACTION = '30 / 7 / 5.5 / 1'
 HYPER = '256 / 0.999 / 40'
 
 SIGHT = 1
@@ -211,6 +211,11 @@ def reward_function(params):
     diff_steer = get_diff_steering(steering)
     abs_steer = abs(steering)
 
+    if steps > 0:
+        diff_steps = progress / steps
+    else:
+        diff_steps = 0
+
     # reward
     if all_wheels_on_track and distance < MAX_CENTER:
         # center bonus
@@ -240,12 +245,12 @@ def reward_function(params):
         #     reward *= 0.5
 
         # progress bonus
-        if steps > 0 and steps <= max_steps:
-            reward += ((progress * 2) / steps)
+        if diff_steps > 0 and steps <= max_steps:
+            reward += (diff_steps * 2)
 
-        # # progress bonus
-        # if diff_progress > 0:
-        #     reward += (diff_progress * 2)
+        # progress bonus
+        if diff_progress > 0 and steps <= max_steps:
+            reward += (diff_progress * 2)
 
     # total reward
     g_total += reward
@@ -260,6 +265,7 @@ def reward_function(params):
     params['diff_progress'] = diff_progress
     params['diff_angle'] = diff_angle
     params['diff_steer'] = diff_steer
+    params['diff_steps'] = diff_steps
     params['abs_steer'] = abs_steer
     params['reward'] = reward
     params['total'] = g_total
