4a13616fe1e9987ac35efe182778db219b78167f
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-07-01 13:08:13 +0900

    re03-80-c

--- 00 reinvent/re03-80-c.py
+++ 00 reinvent/re03-80-c.py
@@ -2,7 +2,7 @@ import json
 import math
 import time
 
-NAME = 're03-80-g'
+NAME = 're03-80-c'
 ACTION = '24 / 5 / 8.0 / 2'
 HYPER = '256 / 0.00003 / 40'
 
@@ -27,24 +27,17 @@ g_waypoints = []
 g_steer = []
 g_total = float(0)
 g_start = float(0)
-g_param = []
 
 
 def get_episode(steps, progress):
     global g_episode
     global g_max_steps
     global g_progress
-    global g_param
 
     # reset
     if steps == 0:
         g_episode += 1
         diff_progress = 0.00001
-
-        if g_episode > 1:
-            g_param['diff_progress'] = g_param['progress']
-            g_param['progress'] = -1
-            print(json.dumps(g_param))
     else:
         diff_progress = progress - g_progress
 
@@ -162,7 +155,6 @@ def reward_function(params):
     global g_steer
     global g_total
     global g_start
-    global g_param
 
     steps = params['steps']
     progress = params['progress']
@@ -227,10 +219,9 @@ def reward_function(params):
 
     # reward
     if distance_from_center < MAX_CENTER and speed > MIN_SPEED:
-        reward = 1.0
-
         # center bonus
-        reward += (BASE_REWARD - (distance / MAX_CENTER))
+        # reward += (BASE_REWARD - (distance / MAX_CENTER))
+        reward = 1.0
 
         if distance < (MAX_CENTER * 0.3):
             reward *= 2.0
@@ -243,37 +234,23 @@ def reward_function(params):
         # if diff_steer <= MAX_STEER:
         #     reward += (BASE_REWARD - (diff_steer / MAX_STEER))
 
-        # # progress bonus
-        # if diff_progress > (90 / max_steps):
-        #     reward += 1.0
+        # progress bonus
+        if diff_progress > 0.7:
+            reward += 1.0
 
         # speed bonus
         if speed > MAX_SPEED:
             reward *= 2.0
-
-        # steer bonus
-        if closest_waypoint >= 10 and closest_waypoint <= 24 and steering >= 0:  # left
+        elif closest_waypoint >= 11 and closest_waypoint <= 24:
             reward *= 1.0
-        elif closest_waypoint >= 33 and closest_waypoint <= 34 and steering <= 0:  # right
+        elif closest_waypoint >= 41 and closest_waypoint <= 42:
             reward *= 1.0
-        elif closest_waypoint >= 40 and closest_waypoint <= 42 and steering >= 0:  # left
+        elif closest_waypoint >= 51 and closest_waypoint <= 52:
             reward *= 1.0
-        elif closest_waypoint >= 50 and closest_waypoint <= 52 and steering >= 0:  # left
-            reward *= 1.0
-        elif closest_waypoint >= 60 and closest_waypoint <= 67 and steering >= 0:  # left
-            reward *= 1.0
-        elif abs_steer <= MAX_STEER:
+        elif closest_waypoint >= 62 and closest_waypoint <= 67:
             reward *= 1.0
         else:
             reward *= 0.1
-
-        # # steer panelity
-        # if abs_steer > MAX_STEER:
-        #     reward *= 0.5
-
-        # # steps panelity
-        # if steps > max_steps:
-        #     reward *= 0.5
 
     # total reward
     g_total += reward
@@ -295,8 +272,6 @@ def reward_function(params):
     params['total'] = g_total
     params['time'] = lap_time
     print(json.dumps(params))
-
-    g_param = params
 
     return float(reward)
 
