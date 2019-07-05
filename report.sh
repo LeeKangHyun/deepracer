01cab26d8c4ef36d67208169bee50b18cb74b481
nalbam <me@nalbam.com>
2019-07-05 22:00:08 +0900

    report

---
+++ report.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+SHELL_DIR=$(dirname $0)
+
+URLS=${SHELL_DIR}/urls.txt
+COUNT=$(cat ${URLS} | wc -l | xargs)
+
+_prepare() {
+    mkdir -p ${SHELL_DIR}/build
+    mkdir -p ${SHELL_DIR}/target
+
+    if [ -f ${SHELL_DIR}/build/points.log ]; then
+        rm -rf ${SHELL_DIR}/build/points.log
+    fi
+}
+
+_build() {
+    IDX=1
+    while read URL; do
+        curl -sL ${URL} | jq -r '.items[].item | "\"\(.additionalFields.racerName)\" \(.additionalFields.lapTime) \(.additionalFields.points)"' > ${SHELL_DIR}/build/board_${IDX}_100.log
+        # curl -sL ${URL} | jq -r '.items[].item | "\"\(.additionalFields.racerName)\" \(.additionalFields.lapTime)"' | head -20 > ${SHELL_DIR}/build/board_${IDX}_20.log
+
+        IDX=$(( ${IDX} + 1 ))
+    done < ${URLS}
+
+    while read LINE; do
+        ARR=(${LINE})
+
+        NAME="$(echo ${ARR[0]} | cut -d'"' -f2)"
+        TIME="${ARR[1]}"
+        POINTS="${ARR[2]}"
+
+        # echo "${NAME} ${TIME}"
+
+        for IDX in {2..3}; do
+            ARR=($(cat ${SHELL_DIR}/build/board_${IDX}_100.log | grep "\"${NAME}\""))
+
+            SUB_TIME="${ARR[1]}"
+            SUB_POINTS="${ARR[2]}"
+
+            if [ "${SUB_TIME}" != "" ]; then
+                if [ "${SUB_POINTS}" == "null" ]; then
+                    # SUB_POINTS="${ARR[1]:3}"
+                    SUB_POINTS=$(perl -e "print 1000-${ARR[1]:3}")
+                fi
+
+                # POINTS=$(( ${POINTS} + ${SUB_POINTS} ))
+                POINTS=$(perl -e "print ${POINTS}+${SUB_POINTS}")
+            fi
+        done
+
+        echo "${POINTS} ${NAME}" >> ${SHELL_DIR}/build/points.log
+
+    done < ${SHELL_DIR}/build/board_1_100.log
+}
+
+_slack() {
+    if [ -z ${SLACK_TOKEN} ]; then
+        return
+    fi
+
+    echo "*DeepRacer Virtual Circuit*" > ${SHELL_DIR}/target/message.log
+    cat ${SHELL_DIR}/build/points.log | sort -r --version-sort | head -20 | column -t | nl >> ${SHELL_DIR}/target/message.log
+
+    json="{\"text\":\"$(cat ${SHELL_DIR}/target/message.log)\"}"
+
+    webhook_url="https://hooks.slack.com/services/${SLACK_TOKEN}"
+    curl -s -d "payload=${json}" "${webhook_url}"
+}
+
+_prepare
+
+_build
+
+_slack
