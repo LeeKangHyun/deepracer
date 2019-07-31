5214599ead7ee7683573199c6746d2ffc393f498
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-07-31 17:40:14 +0900

    add

---
+++ 00 oval/spin.py
@@ -0,0 +1,13 @@
+def reward_function(params):
+    progress = params['progress']
+    steering = params['steering_angle']
+
+    reward = 1e-3
+
+    if steering < 0:
+        reward = 1.0
+
+    if progress == 100:
+        reward = 1000.0
+
+    return float(reward)
